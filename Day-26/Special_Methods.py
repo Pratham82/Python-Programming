2c36212f4580c77b6441dac2c6e8ec35908e330f
Pratham82 <mali.prathamesh82@gmail.com>
2020-05-10 12:57:17 +0530

    Added special del method in class

--- Day-26/Special_Methods.py
+++ Day-26/Special_Methods.py
@@ -11,6 +11,11 @@ class Book():
     # String representation
     def __str__(self):
         return f"Book: {self.title}, Author: {self.author}, Pages: {self.pages}"
+    
+    # Special method for delete
+    # The following method with do the things which are mentioned when the object is delted
+    def __del__(self):
+        print("Book object is deleted")
 
 book1 = Book("Think and grow rich", "napoleon Hill", 200)
 
@@ -26,3 +31,6 @@ print(len(book1))
 # When we are using print method with our object it normally returns a string representation
 # but with the book1 object which is an object of user defined it will only give the object's memory location for getting actual string representation we have to use __str__ method in class Book
 print(book1)
+
+# Delting a variabale
+del book1
