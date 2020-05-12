6c700fd9381ab5df367e7325d6149db23efe903f
Pratham82 <mali.prathamesh82@gmail.com>
2020-05-12 16:23:04 +0530

    Pylint and unit testing

---
+++ Day-29/cap.py
@@ -0,0 +1,6 @@
+def cap(text):
+    '''
+    Input a string 
+    Output the capitalized string
+    '''
+    return text.title()
---
+++ Day-29/simple_script.py
@@ -0,0 +1,18 @@
+'''
+A simple script for printing numbers
+'''
+
+def func1():
+    '''
+    func1 is simple methdo which shows the number which are entered inside.
+    '''
+    first_num = 1 
+    second_num = 2
+    print(first_num)
+    print(second_num)
+
+func1()
+
+# When we run this program using pylint then we can get our code evaluted.
+# It will be used when we'll be working with big projects to generate reports
+# For execution: pylint filename.py
