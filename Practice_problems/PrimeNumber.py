970beeeba41c14adc542c5e3d04eb72cf7ff58a0
Pratham82 <mali.prathamesh82@gmail.com>
2020-05-12 17:11:53 +0530

    Files organized according to the modules

--- Practice_problems/PrimeNumber.py
+++ Practice_problems/PrimeNumber.py
@@ -1,16 +1,8 @@
-num1 = int(input("Enter number: "))
+num = int(input("Enter number for checking: "))
 
-def PrimeChecker(num1):
-    if num1>1:
-        for i in range(2,num1):
-            if (num1 % i)==0:
-                print(num1," not prime number")
-                break
-        else:
-            print(num1," is a prime number")
-    else:
-        print(num1," is not prime number")
-
-# Checking number using created function
-PrimeChecker(num1)
-
+for i in range(2,num):
+    if num % i == 0:
+        print (num, "is a not prime number")
+        break
+else:
+    print (num, "is a prime number")
