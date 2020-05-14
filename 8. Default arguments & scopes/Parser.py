3e14c9c3fc34fb739661c5ed52561ad113847bdb
Pratham82 <mali.prathamesh82@gmail.com>
2020-05-14 19:50:48 +0530

    argparse example

---
+++ 8. Default arguments & scopes/Parser.py
@@ -0,0 +1,34 @@
+import argparse
+
+if __name__ == "__main__":
+    # Initialize the parser
+    parser = argparse.ArgumentParser(
+        description="Parser script"
+    )
+
+    # Adding parameters (positional)
+    # parser.add_argument("num1", help="Number 1", type=float)
+    # parser.add_argument("num2", help="Number 2", type=float)
+    # parser.add_argument("operators", help="Provide operator")
+
+    # Adding parameters (optional) (either -n or --num1 can be used as arguments)
+    parser.add_argument('-n1', "--num1", help="Number 1", type=float)
+    parser.add_argument('-n2', "--num2", help="Number 2", type=float)
+    parser.add_argument('-o', "--operators", help="Provide operator", default="+")
+
+    # parse the arguments
+    args = parser.parse_args()
+    print(args)
+    result = None
+    if args.operators == '+':
+        result = args.num1 + args.num2
+    if args.operators == '-':
+        result = args.num1 - args.num2
+    if args.operators == '*':
+        result = args.num1 * args.num2
+    if args.operators == '/':
+        result = args.num1 / args.num2
+    if args.operators == '%':
+        result = args.num1 & args.num2
+
+    print(result)
