4b2011c72641146eb69327fefa81e9d9dddec395
Pratham82 <mali.prathamesh82@gmail.com>
2020-06-01 16:41:48 +0530

    Added 3 questions

---
+++ Exercises from books/Learn python the hard way/Exercise_29.py
@@ -0,0 +1,22 @@
+people = 40
+cats =  30
+dogs = 15
+
+if people < cats:
+    print("Too many cats! The world is doomed!")
+if people > cats:
+    print("Not many cats! The world is saved!")
+if people < dogs: 
+    print("The world is drooled on!")
+if people > dogs:
+    print("The world is dry!")
+
+dogs += 5
+
+if people >=  dogs:
+    print("People are greater than equal to dogs.")
+if people <= dogs:
+    print("People are lesser than equal to dogs.")
+
+if people == dogs:
+    print("People are dogs.")
