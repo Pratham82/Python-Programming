2b6b385f5a39e70117fa5c2ed9e129490fa056ee
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-06 11:14:32 +0530

    Added API for fetching realtime values

--- Practice_problems/covid_deathrate_calculator.py
+++ Practice_problems/covid_deathrate_calculator.py
@@ -1,12 +1,25 @@
+import requests
+import json
+
 # Program to find Covid DeathRate
 """Program to calculate covid deathrate"""
 # Requirements:
 # 1. Values must be returned by a function.
 # 2. Take user input, total cases and no. of deaths
 
-# Taking input
-total_cases_input = int(input('Enter the total cases:'))
-total_deaths_input = int(input('Enter the total deaths:'))
+
+def get_summary():
+    """Fetching Realtime values"""
+    url = "https://api.covid19api.com/summary"
+    response = requests.get(url)
+    result = json.loads(response.text)
+    total_cases_fetched = result["Global"]["TotalConfirmed"]
+    total_deaths_fetched = result["Global"]["TotalDeaths"]
+    summary = {
+        "total_cases": total_cases_fetched,
+        "total_deaths": total_deaths_fetched
+    }
+    return summary
 
 
 def deathrate_calculator(total_cases, deaths):
@@ -15,5 +28,7 @@ def deathrate_calculator(total_cases, deaths):
     return f"DeathRate calulated from the given values: {round(deathrate,2)}%"
 
 
-print(deathrate_calculator(700000, 350000))
-print(deathrate_calculator(total_cases_input, total_deaths_input))
+total_cases = get_summary()["total_cases"]
+total_deaths = get_summary()["total_deaths"]
+
+print(deathrate_calculator(total_cases, total_deaths))
