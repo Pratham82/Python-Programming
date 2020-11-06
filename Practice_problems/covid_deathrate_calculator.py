f6d759fec0b500336f17cad01f7659ba6aeba105
Prathamesh Mali <mali.prathamesh82@gmail.com>
2020-11-06 10:17:49 +0530

    Fixed grammatical errors

--- Practice_problems/covid_deathrate_calculator.py
+++ Practice_problems/covid_deathrate_calculator.py
@@ -1,19 +1,19 @@
 # Program to find Covid DeathRate
-
+"""Program to calculate covid deathrate"""
 # Requirements:
 # 1. Values must be returned by a function.
 # 2. Take user input, total cases and no. of deaths
 
-# deathrate_calculator
-
-total_cases = int(input('Enter the of total cases:'))
-total_deaths = int(input('Enter the of total deaths:'))
+# Taking input
+total_cases_input = int(input('Enter the total cases:'))
+total_deaths_input = int(input('Enter the total deaths:'))
 
 
 def deathrate_calculator(total_cases, deaths):
+    """Deathrate calculator"""
     deathrate = deaths / total_cases * 100
     return f"DeathRate calulated from the given values: {round(deathrate,2)}%"
 
 
 print(deathrate_calculator(700000, 350000))
-print(deathrate_calculator(total_cases, total_deaths))
+print(deathrate_calculator(total_cases_input, total_deaths_input))
