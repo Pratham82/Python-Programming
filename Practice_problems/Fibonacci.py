7fe037b3577b8330f871fe0579ee0801512d2b2d
pratham82 <mali.prathamesh82@gmail.com>
2021-01-19 21:43:19 +0530

    added formatting

--- Practice_problems/Fibonacci.py
+++ Practice_problems/Fibonacci.py
@@ -1,9 +1,10 @@
 def fib(num):
-    a,b = 1,1
-    while(a<num):
-        print(a,end=" ")
-        a,b = b , a+b
+    a, b = 1, 1
+    while a < num:
+        print(a, end=" ")
+        a, b = b, a + b
+
 
 fib(100)
 
-#a =1
+# a =1
