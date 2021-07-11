f20e8247111da45d24f12fbf4c863b94b8795bb9
pratham82 <mali.prathamesh82@gmail.com>
2021-07-12 01:04:49 +0530

    Added new counter example with optional arguments

--- Important Concepts/Conunter/counter.py
+++ Important Concepts/Conunter/counter.py
@@ -35,7 +35,7 @@ for letter in word:
     counter_3[letter] += 1
 print(counter_3)
 
-### Counter
+### Counter ###
 """
 Input parameters: Iterable or hashable objects 
 Counter internally iterates through the input sequence, counts the number of times a given object occurs, and stores objects as keys and the counts as values.
@@ -51,3 +51,53 @@ print(counter_5)
 
 # Creating couter instances with starting values
 letters = Counter({"i": 4, "s": 4, "p": 2, "m": 1})
+
+# Updating object counts
+# by passing another iterable
+letters.update('missouri')
+print(letters)
+# Counter({'i': 6, 's': 6, 'p': 2, 'm': 2, 'o': 1, 'u': 1, 'r': 1})
+
+# Updating by passing anohter counter
+letters.update(Counter({"i": 4, "s": 4, "p": 2, "m": 1}))
+print(letters)
+# Counter({'i': 10, 's': 10, 'p': 4, 'm': 3, 'o': 1, 'u': 1, 'r': 1})
+
+### Accessing content in the counter ###
+"""
+    1. Similar to using a dictionary key value pairs
+    2. Using .keys() method to access the keys
+    3. Using .values() method to access the values
+    4. Using .items() method to access the items
+"""
+
+new_word = Counter(('intuition'))
+print(f'new_word => {new_word}')
+for key in new_word.keys():
+    print(key)
+
+# Accessing the values
+for value in new_word.values():
+    print(value)
+
+# Accessing both at the same time
+for key, value in new_word.items():
+    print(f'key => {key}, value => {value}')
+
+### Finding most common objects ###
+"""
+If you need to list a group of objects according to their frequency, you can use .most_common(). 
+This method returns a list of (object, count) . 
+"""
+
+sales = Counter(banana=15, tomato=4, apple=39, orange=30)
+
+print(sales.most_common())
+# [('apple', 39), ('orange', 30), ('banana', 15), ('tomato', 4)]
+
+print(sales.most_common(1))
+# [('apple', 39)]
+
+print(sales.most_common(10))
+# If the provided argument is greater than current counter's length it will return all objects
+# [('apple', 39), ('orange', 30), ('banana', 15), ('tomato', 4)]
