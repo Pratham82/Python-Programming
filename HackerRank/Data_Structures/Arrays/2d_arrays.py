a78f9fe075fa4af457a476926c5bdf32d8d7e35c
pratham82 <mali.prathamesh82@gmail.com>
2021-08-21 22:56:35 +0530

    Added 2d arrays soln

---
+++ HackerRank/Data_Structures/Arrays/2d_arrays.py
@@ -0,0 +1,50 @@
+
+# Print the largest (maximum) hourglass sum found in .
+
+# Sample Input
+# 
+# 1 1 1 0 0 0
+# 0 1 0 0 0 0
+# 1 1 1 0 0 0
+# 0 0 2 4 4 0
+# 0 0 0 2 0 0
+# 0 0 1 2 4 0
+# Sample Output
+# 
+# 19
+
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    # Write your code here
+    for i,sub_arr in enumerate(arr):
+        print(sub_arr[3:][:3])
+
+    # print(arr)
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    # fptr.write(str(result) + '\n')
+    # fptr.close()
+
+
