12a6dc64e902882f3b29cd812bc2c381a0ab7190
Prathamesh Mali <prathamesh@rideblitz.com>
2026-04-26 17:42:41 +0530

    feat: 🎸 init cli project

---
+++ anthropic/building-with-claude-api/7. MCP/cli_project/mcp_client.py
@@ -0,0 +1,91 @@
+import sys
+import asyncio
+from typing import Optional, Any
+from contextlib import AsyncExitStack
+from mcp import ClientSession, StdioServerParameters, types
+from mcp.client.stdio import stdio_client
+
+
+class MCPClient:
+    def __init__(
+        self,
+        command: str,
+        args: list[str],
+        env: Optional[dict] = None,
+    ):
+        self._command = command
+        self._args = args
+        self._env = env
+        self._session: Optional[ClientSession] = None
+        self._exit_stack: AsyncExitStack = AsyncExitStack()
+
+    async def connect(self):
+        server_params = StdioServerParameters(
+            command=self._command,
+            args=self._args,
+            env=self._env,
+        )
+        stdio_transport = await self._exit_stack.enter_async_context(
+            stdio_client(server_params)
+        )
+        _stdio, _write = stdio_transport
+        self._session = await self._exit_stack.enter_async_context(
+            ClientSession(_stdio, _write)
+        )
+        await self._session.initialize()
+
+    def session(self) -> ClientSession:
+        if self._session is None:
+            raise ConnectionError(
+                "Client session not initialized or cache not populated. Call connect_to_server first."
+            )
+        return self._session
+
+    async def list_tools(self) -> list[types.Tool]:
+        # TODO: Return a list of tools defined by the MCP server
+        return []
+
+    async def call_tool(
+        self, tool_name: str, tool_input: dict
+    ) -> types.CallToolResult | None:
+        # TODO: Call a particular tool and return the result
+        return None
+
+    async def list_prompts(self) -> list[types.Prompt]:
+        # TODO: Return a list of prompts defined by the MCP server
+        return []
+
+    async def get_prompt(self, prompt_name, args: dict[str, str]):
+        # TODO: Get a particular prompt defined by the MCP server
+        return []
+
+    async def read_resource(self, uri: str) -> Any:
+        # TODO: Read a resource, parse the contents and return it
+        return []
+
+    async def cleanup(self):
+        await self._exit_stack.aclose()
+        self._session = None
+
+    async def __aenter__(self):
+        await self.connect()
+        return self
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.cleanup()
+
+
+# For testing
+async def main():
+    async with MCPClient(
+        # If using Python without UV, update command to 'python' and remove "run" from args.
+        command="uv",
+        args=["run", "mcp_server.py"],
+    ) as _client:
+        pass
+
+
+if __name__ == "__main__":
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+    asyncio.run(main())
---
+++ anthropic/building-with-claude-api/7. MCP/cli_project/mcp_server.py
@@ -0,0 +1,24 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("DocumentMCP", log_level="ERROR")
+
+
+docs = {
+    "deposition.md": "This deposition covers the testimony of Angela Smith, P.E.",
+    "report.pdf": "The report details the state of a 20m condenser tower.",
+    "financials.docx": "These financials outline the project's budget and expenditures.",
+    "outlook.pdf": "This document presents the projected future performance of the system.",
+    "plan.md": "The plan outlines the steps for the project's implementation.",
+    "spec.txt": "These specifications define the technical requirements for the equipment.",
+}
+
+# TODO: Write a tool to read a doc
+# TODO: Write a tool to edit a doc
+# TODO: Write a resource to return all doc id's
+# TODO: Write a resource to return the contents of a particular doc
+# TODO: Write a prompt to rewrite a doc in markdown format
+# TODO: Write a prompt to summarize a doc
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
