1631201e479781971d2f8e996d03c6f4135af71c
Prathamesh Mali <prathamesh@rideblitz.com>
2026-04-26 19:03:56 +0530

    feat: 🎸 add new functions in client

--- anthropic/building-with-claude-api/7. MCP/cli_project/mcp_client.py
+++ anthropic/building-with-claude-api/7. MCP/cli_project/mcp_client.py
@@ -42,14 +42,13 @@ class MCPClient:
         return self._session
 
     async def list_tools(self) -> list[types.Tool]:
-        # TODO: Return a list of tools defined by the MCP server
-        return []
+        result = await self.session().list_tools()
+        return result.tools
 
     async def call_tool(
         self, tool_name: str, tool_input: dict
     ) -> types.CallToolResult | None:
-        # TODO: Call a particular tool and return the result
-        return None
+        return await self.session().call_tool(tool_name, tool_input)
 
     async def list_prompts(self) -> list[types.Prompt]:
         # TODO: Return a list of prompts defined by the MCP server
@@ -82,7 +81,8 @@ async def main():
         command="uv",
         args=["run", "mcp_server.py"],
     ) as _client:
-        pass
+        result = await _client.list_tools()
+        print(result)
 
 
 if __name__ == "__main__":
