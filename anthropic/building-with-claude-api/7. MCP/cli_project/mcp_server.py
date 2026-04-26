2a5afffb494ae8fe8ee1b0728f990b6b035e3d23
Prathamesh Mali <prathamesh@rideblitz.com>
2026-04-26 17:43:13 +0530

    feat: 🎸 defining read and write tools with mcp

--- anthropic/building-with-claude-api/7. MCP/cli_project/mcp_server.py
+++ anthropic/building-with-claude-api/7. MCP/cli_project/mcp_server.py
@@ -13,7 +13,34 @@ docs = {
 }
 
 # TODO: Write a tool to read a doc
+@mcp.tool(
+    name="read_doc_contents",
+    description="Read the content of a document and return it as a string"
+)
+def read_document(
+    doc_id: str = Field(description="Id od the document to read")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found")
+
+    return docs[doc_id]
+    
+
 # TODO: Write a tool to edit a doc
+@mcp.tool(
+    name="edit_document",
+    description="Edit a document by replacing a string in the documents content with a new string."
+)
+def edit_document(
+    doc_id: str = Field(description="Id of the document that will be edited"),
+    old_str: str = Field(description="The text to replace. Must match exactly, including whitespace."),
+    new_str: str = Field(description="The new text to insert in place of the old text.")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found")
+    
+    docs[doc_id] = docs[doc_id].replace(old_str, new_str)
+
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
